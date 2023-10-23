d0535fa2fd4d25f52806e6deff2b01c47d696bee
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2023-10-23 22:07:06 +0300

    +

--- Lab2/ex5.py
+++ Lab2/ex5.py
@@ -1,7 +1,9 @@
 # 5
 def main_diag(a):
-    for i in range(len(a)-1):
-        a[i+1][i] = 0
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            if i > j:
+                a[i][j] = 0
     return a
 
 
